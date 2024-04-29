4e4b908e95db8bd2692d2c5b67a2752f16561d8d
pikounis <anastasiospikounis@gmail.com>
2024-04-29 16:32:41 +0100

    added verification to my name and kept old code as comment

--- src/Routes/Home/components/AboutMe/AboutMe.jsx
+++ src/Routes/Home/components/AboutMe/AboutMe.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './AboutMe.css';
 import {Container, Grid, Typography, Box, Button} from "@mui/material";
+import VerifiedIcon from '@mui/icons-material/Verified';
+import { blue } from '@mui/material/colors';
 import Photo from "../Photo/Photo.jsx";
 import {useTranslation} from "react-i18next";
 
@@ -13,11 +15,27 @@ function AboutMe() {
                     <Grid item xs={12} md={6} container justifyContent="center" alignItems="center">
                         <Photo />
                     </Grid>
+                    {/*<Grid item xs={12} md={6}>*/}
+                    {/*    <Typography variant="h2" className="aboutme-title" >Anastasios Pikounis</Typography>*/}
+                    {/*    <Typography variant="body1"className="aboutme-title" >*/}
+                    {/*        Software Engineer*/}
+                    {/*    </Typography>*/}
+                    {/*</Grid>*/}
                     <Grid item xs={12} md={6}>
-                        <Typography variant="h2" className="aboutme-title" >Anastasios Pikounis</Typography>
-                        <Typography variant="body1"className="aboutme-title" >
-                            Software Engineer
-                        </Typography>
+                        <Box>
+                            {/* Name and Verified Icon on the same line */}
+                            <Box display="flex" alignItems="center">
+                                <Typography variant="h2" className="aboutme-title" sx={{ marginRight: 1 }}>
+                                    Anastasios Pikounis
+                                </Typography>
+                                <VerifiedIcon sx={{ color: blue[500], fontSize: '1.5rem' }} />
+                            </Box>
+
+                            {/* Job Title below Name and Icon */}
+                            <Typography variant="body1" className="aboutme-title">
+                                Software Engineer
+                            </Typography>
+                        </Box>
                     </Grid>
                     <Box sx={{ height: '50px', width: '100%' }}></Box>
                     <Grid item xs={12} md={12} className="last-grid-item">
